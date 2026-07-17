ba316018b4e2e065775127887bff4b71e4f62baf
walk6834 <glacier0315@126.com>
2026-07-17 11:03:15 +0800

    chore(collect_upload.sh): 移除多余的.config文件复制步骤
    删除了脚本中手动复制.config文件的冗余代码，保留find移动固件镜像的逻辑

--- x86-64/custom_scripts/collect_upload.sh
+++ x86-64/custom_scripts/collect_upload.sh
@@ -2,8 +2,6 @@
 
 mkdir -p "$GITHUB_WORKSPACE/$UPLOAD_DIR"
 
-cp -f .config "$GITHUB_WORKSPACE/$UPLOAD_DIR/"
-
 # 查找并移动所有匹配的 img.gz 文件
 find ./bin/targets/ -type f -name "*wrt*.img.gz" -exec mv {} "$GITHUB_WORKSPACE/$UPLOAD_DIR/" \; 2>/dev/null
 
