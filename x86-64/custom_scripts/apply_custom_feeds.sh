b3078002c1a9dc443107c03cec62ca3e862e0a11
walk6834 <glacier0315@126.com>
2026-07-18 00:14:01 +0800

    chore(x86-64/custom_scripts): 更新自定义feeds配置与插件清理
    调整了自定义feeds源的添加顺序，替换原smpackage源为kenzo的openwrt-packages和small源，同时更新了冲突/冗余插件的清理规则，新增golang 1.26版本的克隆安装逻辑

--- x86-64/custom_scripts/apply_custom_feeds.sh
+++ x86-64/custom_scripts/apply_custom_feeds.sh
@@ -35,10 +35,12 @@ update_install_feeds() {
     ./scripts/feeds update -a
 
     echo "删除冲突的插件..."
-    rm -rf feeds/smpackage/{base-files,dnsmasq,firewall*,fullconenat,libnftnl,nftables,ppp,opkg,ucl,upx,vsftpd*,miniupnpd-iptables,wireless-regdb}
+    rm -rf feeds/luci/applications/luci-app-mosdns
+    rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
 
-    echo "删除报错的插件..."
-    rm -rf feeds/smpackage/{webd}
+    echo "安装golang..."
+    rm -rf feeds/packages/lang/golang
+    git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
     
     echo "安装feeds..."
     ./scripts/feeds install -a
@@ -47,7 +49,8 @@ update_install_feeds() {
 # 主函数
 main() {
     # 添加自定义Feeds
-    append_feed "src-git smpackage https://github.com/kenzok8/small-package;main"
+    insert_feed "src-git kenzo https://github.com/kenzok8/openwrt-packages" 1
+    insert_feed "src-git small https://github.com/kenzok8/small" 2
 
     # 更新&安装插件
     update_install_feeds
