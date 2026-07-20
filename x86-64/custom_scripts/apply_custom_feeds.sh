32a36fe1d17771fa5955d34e482b66e4d8afdfa5
walk6834 <glacier0315@126.com>
2026-07-20 22:26:00 +0800

    refactor(apply_custom_feeds.sh): 优化脚本结构与缩进格式
    1. 新增通用目录查找函数find_dir
    2. 统一所有代码缩进为tab规范
    3. 清理多余空行并调整注释格式

--- x86-64/custom_scripts/apply_custom_feeds.sh
+++ x86-64/custom_scripts/apply_custom_feeds.sh
@@ -1,18 +1,25 @@
 #!/bin/sh
 
+# 查找目录
+find_dir() {
+	dir="$1"
+	name="$2"
+	find "$dir" -maxdepth 3 -type d -name "$name" -print -quit 2>/dev/null
+}
+
 # 插入自定义Feeds
 add_feed() {
-    local feed_line="$1"
-    local insert_line="${2:-\$a}"
+	feed_line="$1"
+	insert_line="${2:-\$a}"
 
-    # 已存在则跳过
-    if grep -qF "$feed_line" feeds.conf.default; then
-        echo "[跳过] 已存在: $feed_line"
-        return
-    fi
+	# 已存在则跳过
+	if grep -qF "$feed_line" feeds.conf.default; then
+		echo "[跳过] 已存在: $feed_line"
+		return
+	fi
 
-    sed -i "${insert_line} $feed_line" feeds.conf.default
-    echo "[新增] $feed_line"
+	sed -i "${insert_line} $feed_line" feeds.conf.default
+	echo "[新增] $feed_line"
 }
 
 # # 更新&安装插件
@@ -27,7 +34,7 @@ add_feed() {
 #     echo "安装golang..."
 #     rm -rf feeds/packages/lang/golang
 #     git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
-    
+
 #     echo "安装feeds..."
 #     ./scripts/feeds install -a
 # }
@@ -44,54 +51,53 @@ add_feed() {
 
 # 添加自定义Feeds
 add_custom_feeds() {
-    add_feed "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" '1i'
-    add_feed "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" '2i'
-    add_feed "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main"
-    add_feed "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main"
+	add_feed "src-git passwall_packages https://github.com/Openwrt-Passwall/openwrt-passwall-packages.git;main" '1i'
+	add_feed "src-git passwall_luci https://github.com/Openwrt-Passwall/openwrt-passwall.git;main" '2i'
+	add_feed "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main"
+	add_feed "src-git momo https://github.com/nikkinikki-org/OpenWrt-momo.git;main"
 }
 
 # 更新&安装插件
 update_install_feeds() {
-    echo "更新feeds..."
-    ./scripts/feeds update -a
+	echo "更新feeds..."
+	./scripts/feeds update -a
 
-    echo "安装feeds..."
-    ./scripts/feeds install -a
+	echo "安装feeds..."
+	./scripts/feeds install -a
 }
 
 # 添加自定义Feeds
 add_custom_feeds1() {
-    # 添加自定义Feeds
-    add_feed "src-git kenzo https://github.com/kenzok8/openwrt-packages" '1i'
-    add_feed "src-git small https://github.com/kenzok8/small" '2i'
+	# 添加自定义Feeds
+	add_feed "src-git kenzo https://github.com/kenzok8/openwrt-packages" '1i'
+	add_feed "src-git small https://github.com/kenzok8/small" '2i'
 }
 
 # 更新&安装插件
 update_install_feeds1() {
-    echo "更新feeds..."
-    ./scripts/feeds update -a
+	echo "更新feeds..."
+	./scripts/feeds update -a
 
-    echo "删除冲突的插件..."
-    rm -rf feeds/luci/applications/luci-app-mosdns
-    rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
+	echo "删除冲突的插件..."
+	rm -rf feeds/luci/applications/luci-app-mosdns
+	rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns} feeds/packages/utils/v2dat feeds/packages/lang/golang
 
-    echo "安装golang..."
-    rm -rf feeds/packages/lang/golang
-    git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
-    
-    echo "安装feeds..."
-    ./scripts/feeds install -a
+	echo "安装golang..."
+	rm -rf feeds/packages/lang/golang
+	git clone https://github.com/kenzok8/golang -b 1.26 feeds/packages/lang/golang
+
+	echo "安装feeds..."
+	./scripts/feeds install -a
 }
 
 # 主函数
 main() {
-    # 添加自定义Feeds
-    add_custom_feeds
+	# 添加自定义Feeds
+	add_custom_feeds
 
-    # 更新&安装插件
-    update_install_feeds
+	# 更新&安装插件
+	update_install_feeds
 }
 
 # 调用主函数
 main "$@"
-
